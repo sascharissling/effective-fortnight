64ad582d0530ed520a2ad11a8e60c6cca91df375
Sascha Rissling <sascha.rissling@gmail.com>
2020-01-23 11:24:49 +0100

    Enable platform specific rendering of background

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import styled, { css } from "@emotion/native";
+import { Platform } from "react-native";
+
+const AppContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  ${Platform.select({
+    ios: css`
+      background-color: red;
+    `,
+    android: css`
+      background-color: red;
+    `
+  })};
+`;
+
+const Welcome = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 25px;
+  text-align: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <AppContainer>
+      <Welcome>Open up App.js to start working on your app!</Welcome>
+    </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
